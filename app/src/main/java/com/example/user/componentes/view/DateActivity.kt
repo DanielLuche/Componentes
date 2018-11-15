d259e930db43cdee99706c937170832222bf27bf
DanielLuche <d.luche@namoadigital.com>
2018-11-15 12:08:06 -0200

    Finalizado modulo componente adicionando o timepicker

--- app/src/main/java/com/example/user/componentes/view/DateActivity.kt
+++ app/src/main/java/com/example/user/componentes/view/DateActivity.kt
@@ -1,16 +1,19 @@
 package com.example.user.componentes.view
 
 import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.DatePicker
+import android.widget.TimePicker
+import android.widget.Toast
 import com.example.user.componentes.R
 import kotlinx.android.synthetic.main.activity_date.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DateActivity : AppCompatActivity(), View.OnClickListener,DatePickerDialog.OnDateSetListener {
+class DateActivity : AppCompatActivity(), View.OnClickListener,DatePickerDialog.OnDateSetListener, TimePicker.OnTimeChangedListener {
 
     private val mSimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
 
@@ -23,6 +26,10 @@ class DateActivity : AppCompatActivity(), View.OnClickListener,DatePickerDialog.
 
     private fun setListeners(){
         dateActBtnDatePicker.setOnClickListener(this)
+        dateActBtnGetTime.setOnClickListener(this)
+        dateActBtnSetTime.setOnClickListener(this)
+        //
+        dateActTimePicker.setOnTimeChangedListener(this)
     }
 
     override fun onClick(view: View) {
@@ -30,6 +37,27 @@ class DateActivity : AppCompatActivity(), View.OnClickListener,DatePickerDialog.
 
         if(id == R.id.dateActBtnDatePicker){
             openDatePickerDialog()
+        } else if(id == R.id.dateActBtnGetTime){
+            //Aqui acho que faltou format da data, pois quando minuto menor que 10, não aparece com o 0 a direita.
+            //valida versão do android o usr para saber qual codigo usar
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+              val value = "${dateActTimePicker.hour} :${dateActTimePicker.minute}"
+                Toast.makeText(this,value,Toast.LENGTH_SHORT).show()
+            }else{
+                val value = "${dateActTimePicker.currentHour}:${dateActTimePicker.currentMinute}"
+                Toast.makeText(this,value,Toast.LENGTH_SHORT).show()
+            }
+
+        }else if(id == R.id.dateActBtnSetTime){
+            //valida versão do android o usr para saber qual codigo usar
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                dateActTimePicker.hour = 20
+                dateActTimePicker.minute = 15
+            }else{
+                dateActTimePicker.currentHour = 20
+                dateActTimePicker.currentMinute = 15
+            }
+
         }
     }
 
@@ -38,6 +66,10 @@ class DateActivity : AppCompatActivity(), View.OnClickListener,DatePickerDialog.
         calendar.set(year,month,day)
         //
         dateActBtnDatePicker.text = mSimpleDateFormat.format(calendar.time)
+    }
+
+    override fun onTimeChanged(timePicker: TimePicker?, hourOfDay: Int, minute: Int) {
+        Toast.makeText(this,"$hourOfDay:$minute",Toast.LENGTH_SHORT).show()
     }
 
     private fun openDatePickerDialog() {
